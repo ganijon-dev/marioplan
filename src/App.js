eb015d12cd3da138b8da3d51a9db76eb3bbe0008
Ganijon <tgani96@gmail.com>
2020-03-09 19:26:47 +0300

    Files Created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className='center green-text dark-text'> MarioPlan</h1> 
+      <h1 className='center green-text dark-text'> 222ioPlan</h1> 
     </div>
   );
 }
